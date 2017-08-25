efeccc1946b2aa6ff75e4545c588dfc0da472e5a
rkishlay <kishlay.raj07@gmail.com>
2017-08-25 17:58:44 +0530

    updated

--- music/views.py
+++ music/views.py
@@ -16,3 +16,18 @@ def detail(request, album_id):
          raise Http404("You have landed on a unknown album page !")"""
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You didnt selected",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
